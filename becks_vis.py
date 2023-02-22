7e4358f20a524e9cd552e0026f724e0ef92d5e74
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-02-22 14:00:18 -0800

    updated algo now theres no errors

--- becks_vis.py
+++ becks_vis.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 IATACodeHashmap = {}
 try:
-    with open('airport_codes.csv', 'r', encoding='utf-8') as file: 
+    with open('airport_codes.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         headers = next(reader)
         IATACodeHashmap = {}
@@ -14,10 +14,10 @@ try:
             IATACodeHashmap[row[0]] = row[1]
 except Exception as e:
     print("Error:", e)
-    
+
 Data = []
 try:
-    with open('datasets/aviation_comments_submissions_2023.csv', 'r', encoding='utf-8') as file: 
+    with open('datasets/aviation_comments_submissions_2023.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         headers = next(reader)
         for row in reader:
@@ -27,33 +27,45 @@ except Exception as e:
 
 print("data len: " + str(len(Data)))
 
+
 def algo(myData):
     newData = []
     for title_comment in myData:
-      title_comment = " " + title_comment + " "
-      #find all capital 3-letter words
-      endindex = len(title_comment) - 4
-      i = 0
-      while i < endindex:
-            try:
-                #print(str(i) + title_comment[i] + " " + title_comment[i+4])
-                if not title_comment[i].isalnum() and not title_comment[i+4].isalnum(): #check if the character left and right of potential code is nonalphanumeric
-                  potentialCode = title_comment[i+1] + title_comment[i+2] + title_comment[i+3]
-                  if potentialCode.isupper():
-                    fullName = IATACodeHashmap.get(potentialCode)
-                    if fullName!= None: #if there was a match
-                      title_comment = title_comment[0 : i + 1] + fullName + title_comment[i+4 : len(title_comment)]
-                      endindex += (len(fullName) - 2) #adjust the end index 
-                      i += len(fullName) #skip scanning the airport name
+        if (len(title_comment) >= 3):  # filter out titles and comments that are less than 3 characters long (iata codes are 3 characters long)
+
+            title_comment = " " + title_comment + "  "
+            # find all capital 3-letter words
+            endindex = len(title_comment) - 5
+            i = 0
+            # print("comment length" + str(len(title_comment)))
+
+            while i < endindex:
+                # print(len(title_comment))
+                # print(str(i) + title_comment[i] + " " + title_comment[i+4])
+                # check if the character left and right of potential code is nonalphanumeric
+                if not title_comment[i].isalnum() and not title_comment[i+4].isalnum():
+                    potentialCode = title_comment[i+1] + \
+                        title_comment[i+2] + title_comment[i+3]
+                    if potentialCode.isupper():
+                        fullName = IATACodeHashmap.get(potentialCode)
+                        if fullName != None:  # if there was a match
+                            title_comment = title_comment[0: i + 1] + \
+                                fullName + \
+                                title_comment[i+4: len(title_comment)]
+                            # adjust the end index
+                            endindex += (len(fullName) - 3)
+                            # skip scanning the airport name
+                            i += len(fullName)
+                            # print("endindex: " + str(endindex))
+                            # print("i: " + str(i))
                 i += 1
-            except Exception as e:
-                i += 1
-                continue
-      title_comment = title_comment[1 : len(title_comment) - 1] #remove first and last space
-      newData.append(title_comment)
+            # remove first and last space
+            title_comment = title_comment[1: len(title_comment) - 2]
+        newData.append(title_comment)
     return newData
-   
-def tryItABunch(myFn, startN=10, endN=100, stepSize=10, numTrials=20, listMax = 10):
+
+
+def tryItABunch(myFn, startN=10, endN=100, stepSize=10, numTrials=20, listMax=10):
     nValues = []
     tValues = []
     for n in range(startN, endN, stepSize):
@@ -61,18 +73,18 @@ def tryItABunch(myFn, startN=10, endN=100, stepSize=10, numTrials=20, listMax = 
         # run myFn several times and average to get a decent idea.
         runtime = 0
         for t in range(numTrials):
-            lst = Data[:n] # generate a random list of length n
+            lst = Data[:n]  # generate a random list of length n
             start = time.time()
-            myFn( lst )
+            myFn(lst)
             end = time.time()
-            runtime += (end - start) * 1000 # measure in milliseconds
+            runtime += (end - start) * 1000  # measure in milliseconds
         runtime = runtime/numTrials
         nValues.append(n)
         tValues.append(runtime)
     return nValues, tValues
 
 
-nValues, tValues = tryItABunch( algo, startN=10, endN=1000)
+nValues, tValues = tryItABunch(algo, startN=10, endN=1000)
 
 plt.plot(nValues, tValues, color="blue", label="Algo #1")
 plt.xlabel("n")
