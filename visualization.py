77535e4b37ae0035236a00fab064744d59e63afc
kitkat6410 <kjbarnes6410@gmail.com>
2023-03-04 20:12:06 -0800

    fixed error

--- visualization.py
+++ visualization.py
@@ -17,7 +17,7 @@ except Exception as e:
 
 Data = []
 try:
-    with open('datasets/aviation_comments_submissions_2023.csv', 'r', encoding='utf-8') as file:
+    with open('datasets/smaller_data_set.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         headers = next(reader)
         for row in reader:
